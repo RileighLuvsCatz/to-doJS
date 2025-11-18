8328b95db4ef3bee532fbc5e62846ee311b4a980
Rileigh <rdettlingburki@gmail.com>
2025-11-18 16:35:53 -0600

    Refactor program to use Item objects for better structure, clarity and future features

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 //todo
 
+function Item(id, list, text, note) {
+    this.id = id;
+    this.list = list;
+    this.text = text;
+    this.note = note;
+}
+
 let todo = [];
 let completed = [];
 const input = document.getElementById("input");
@@ -10,37 +17,48 @@ function addToList() {
     if (input.value === "") {
         return;
     }
-    todo.push(input.value);
-    const newItem = document.createElement('div');
-    newItem.setAttribute("id",input.value);
+
+    const newItemObject = new Item('item-' + Date.now(),'todo', input.value, "")
+    todo.push(newItemObject);
+
+    const newItemDiv = document.createElement('div');
+    newItemDiv.setAttribute("id",newItemObject.id);
+
     const name = document.createElement('p');
-    name.innerText = "- " + input.value;
+    name.innerText = "- " + newItemObject.text;
+
     const done = document.createElement('button');
-    done.setAttribute("onclick", `addToCompletedList('${input.value}')`);
+    done.setAttribute("onclick", `addToCompletedList('${newItemObject.id}')`);
     done.innerText = "Done!";
+    
     const remove = document.createElement('button');
-    remove.setAttribute("onclick", `deleteItem('${input.value}')`);
+    remove.setAttribute("onclick", `deleteItem('${newItemObject.id}', '${newItemObject.list}')`);
     remove.innerText = "X";
 
-    newItem.appendChild(name);
-    newItem.appendChild(done);
-    newItem.appendChild(remove);
-    list.appendChild(newItem);
+    newItemDiv.appendChild(name);
+    newItemDiv.appendChild(done);
+    newItemDiv.appendChild(remove);
+    list.appendChild(newItemDiv);
     input.value = "";
-    saveToLocalStorage();
+    //saveToLocalStorage();
 }
 
-function deleteItem(id) {
-    const temp = document.getElementById(id);
-    if (completed.includes(id)) {
+function deleteItem(id, listType) {
+    let type = listType;
+    
+    if (type == 'completed') {
+        const temp = document.getElementById(id);
+        completed = completed.filter(item => item.id !== id);
         completedList.removeChild(temp);
-        completed.pop(id);
         saveToLocalStorage();
+        return;
 
-    } else if (todo.includes(id)) {
+    } else if (type == 'todo') {
+        const temp = document.getElementById(id);
+        todo = todo.filter(item => item.id !== id);
         list.removeChild(temp);
-        todo.pop(id);
         saveToLocalStorage();
+        return;
     }
 }
 
@@ -54,11 +72,22 @@ function clearLists() {
 
 function addToCompletedList(id) {
     const temp = document.getElementById(id);
-    completed.push(todo.pop(id));
+    //array manipulation
+    const itemA = todo.find(item => item.id === id);
+    itemA.list = 'completed';
+    completed.push(itemA);
+    todo = todo.filter(item => item.id !== id);
+
+    //move element in DOM
     list.removeChild(temp);
     completedList.appendChild(temp);
+    //remove done button
     const doneButton = temp.querySelector('button');
     temp.removeChild(doneButton);
+    //remap delete button
+    const deleteButton = temp.querySelector('button');
+    deleteButton.setAttribute("onclick", `deleteItem('${id}', 'completed')`);
+
     saveToLocalStorage();
 }
 
@@ -83,14 +112,14 @@ function loadFromLocalStorage() {
 
     for(let item of todo) {
         const newItem = document.createElement('div');
-        newItem.setAttribute("id",item);
+        newItem.setAttribute("id",item.id);
         const name = document.createElement('p');
-        name.innerText = item;
+        name.innerText = '- ' + item.text;
         const done = document.createElement('button');
-        done.setAttribute("onclick", `addToCompletedList('${item}')`);
+        done.setAttribute("onclick", `addToCompletedList('${item.id}')`);
         done.innerText = "Done!";
         const remove = document.createElement('button');
-        remove.setAttribute("onclick", `deleteItem('${item}')`);
+        remove.setAttribute("onclick", `deleteItem('${item.id}', 'todo')`);
         remove.innerText = "X";
 
         newItem.appendChild(name);
@@ -101,16 +130,19 @@ function loadFromLocalStorage() {
     
     for(let item of completed) {
         const newItem = document.createElement('div');
-        newItem.setAttribute("id",item);
+        newItem.setAttribute("id",item.id);
         const name = document.createElement('p');
-        name.innerText = item;
+        name.innerText = "- " + item.text;
         const remove = document.createElement('button');
-        remove.setAttribute("onclick", `deleteItem('${item}')`);
+        remove.setAttribute("onclick", `deleteItem('${item.id}', 'completed')`);
         remove.innerText = "X";
         
         newItem.appendChild(name);
         newItem.appendChild(remove);
         completedList.appendChild(newItem);
     }
+    console.log("Loaded from local storage");
+    console.log(todo);
+    console.log(completed);
 }
 

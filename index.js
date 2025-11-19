6871b39a3b5ffb69b5a0bb008db614aa973683ed
Rileigh <rdettlingburki@gmail.com>
2025-11-18 18:33:33 -0600

    [Feature] Notes Sidebar

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function Item(id, list, text, note) {
     this.text = text;
     this.note = note;
 }
+let sidebarState = 'closed';
 
 let todo = [];
 let completed = [];
@@ -24,23 +25,28 @@ function addToList() {
     const newItemDiv = document.createElement('div');
     newItemDiv.setAttribute("id",newItemObject.id);
 
-    const name = document.createElement('p');
+    const name = document.createElement('button');
     name.innerText = "- " + newItemObject.text;
+    //set id for name to match item id
+    name.setAttribute("id", "name-" + newItemObject.id);
+    name.addEventListener('click', () => openNotesSidebar(newItemObject.id));
 
     const done = document.createElement('button');
-    done.setAttribute("onclick", `addToCompletedList('${newItemObject.id}')`);
+    done.addEventListener('click', () => addToCompletedList(newItemObject.id));
     done.innerText = "Done!";
+    done.setAttribute("id", "done-" + newItemObject.id);
     
     const remove = document.createElement('button');
-    remove.setAttribute("onclick", `deleteItem('${newItemObject.id}', '${newItemObject.list}')`);
+    remove.addEventListener('click', () => deleteItem(newItemObject.id, 'todo'));
     remove.innerText = "X";
+    remove.setAttribute("id", "remove-" + newItemObject.id);
 
     newItemDiv.appendChild(name);
     newItemDiv.appendChild(done);
     newItemDiv.appendChild(remove);
     list.appendChild(newItemDiv);
     input.value = "";
-    //saveToLocalStorage();
+    saveToLocalStorage();
 }
 
 function deleteItem(id, listType) {
@@ -82,11 +88,11 @@ function addToCompletedList(id) {
     list.removeChild(temp);
     completedList.appendChild(temp);
     //remove done button
-    const doneButton = temp.querySelector('button');
+    const doneButton = temp.querySelector('#done-' + id);
     temp.removeChild(doneButton);
     //remap delete button
-    const deleteButton = temp.querySelector('button');
-    deleteButton.setAttribute("onclick", `deleteItem('${id}', 'completed')`);
+    const deleteButton = temp.getElementById('#remove-' + id);
+    deleteButton.addEventListener('click', () => deleteItem(id, 'completed'));
 
     saveToLocalStorage();
 }
@@ -97,6 +103,9 @@ input.addEventListener("keyup", function(event) {
     }
 });
 window.addEventListener('DOMContentLoaded', loadFromLocalStorage());
+document.getElementById('closeBtn').addEventListener('click', closeNotesSidebar);
+document.getElementById('addBtn').addEventListener('click', addToList);
+document.getElementById('clearBtn').addEventListener('click', clearLists);
 
 function saveToLocalStorage() {
     localStorage.setItem("todo", JSON.stringify(todo));
@@ -113,13 +122,14 @@ function loadFromLocalStorage() {
     for(let item of todo) {
         const newItem = document.createElement('div');
         newItem.setAttribute("id",item.id);
-        const name = document.createElement('p');
+        const name = document.createElement('button');
         name.innerText = '- ' + item.text;
+        name.addEventListener('click', () => openNotesSidebar(item.id));
         const done = document.createElement('button');
-        done.setAttribute("onclick", `addToCompletedList('${item.id}')`);
+        done.addEventListener('click', () => addToCompletedList(item.id));
         done.innerText = "Done!";
         const remove = document.createElement('button');
-        remove.setAttribute("onclick", `deleteItem('${item.id}', 'todo')`);
+        remove.addEventListener('click', () => deleteItem(item.id,'todo'));
         remove.innerText = "X";
 
         newItem.appendChild(name);
@@ -131,10 +141,11 @@ function loadFromLocalStorage() {
     for(let item of completed) {
         const newItem = document.createElement('div');
         newItem.setAttribute("id",item.id);
-        const name = document.createElement('p');
+        const name = document.createElement('button');
         name.innerText = "- " + item.text;
+        name.addEventListener('click', () => openNotesSidebar(item.id));
         const remove = document.createElement('button');
-        remove.setAttribute("onclick", `deleteItem('${item.id}', 'completed')`);
+        remove.addEventListener('click', () => deleteItem(item.id, 'completed'));
         remove.innerText = "X";
         
         newItem.appendChild(name);
@@ -146,3 +157,30 @@ function loadFromLocalStorage() {
     console.log(completed);
 }
 
+function openNotesSidebar(id) {
+    if (sidebarState != 'closed') return;
+    sidebarState = "open " + id;
+    
+    let theItem = todo.find(item => item.id === id);
+    if (!theItem) theItem = completed.find(item => item.id === id);
+    
+    const content = theItem.note;
+    const textArea = document.getElementById('notesInput');
+    textArea.value = content;
+    const notesHeader = document.getElementById('notes');
+    notesHeader.innerText = "Notes: " + theItem.text;
+    const hiding = document.getElementById('notes sidebar');
+    hiding.className = "sidebar";
+}
+
+function closeNotesSidebar() {
+
+    let id = sidebarState.slice(5);
+    let theItem = todo.find(item => item.id === id);
+    if (!theItem) theItem = completed.find(item => item.id === id);
+    theItem.note = document.getElementById('notesInput').value;
+    let hiding = document.getElementById('notes sidebar');
+    hiding.className = "sidebar hidden";
+    sidebarState = 'closed';
+    saveToLocalStorage();
+}
